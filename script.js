28f1000368352ada79c85a24cbb9583b583eaf37
Akash Vaibhav <106489299+akash2m@users.noreply.github.com>
2025-02-16 01:33:47 +0530

    script.js

--- script.js
+++ script.js
@@ -1,51 +1,18 @@
-var myNodelist = document.getElementsByTagName("li");
-var j;
-for (j = 0; j < myNodelist.length; j++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[j].appendChild(span);
-}
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
 
-var close = document.getElementsByClassName("close");
-var j;
-for (j = 0; j< close.length; j++) {
-  close[j].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("my").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+function Add(){
+    if(inputs.value == ""){
+        alert("Please Enter Task")
     }
-  }
+    else {
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newEle.remove()
+        }
+    }
 }
